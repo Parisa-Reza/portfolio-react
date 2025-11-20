edfe39f19e7f1f753facf368bb51c1bab44c8e77
Parisa Reza <rezaparisa5@gmail.com>
2025-11-21 00:09:24 +0600

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+export function App() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <h1 className="text-3xl font-bold underline text-purple-600">
+        bongoDev Vite + ReactJS + TailwindCSS Template!
+      </h1>
+    </div>
+  );
+}
